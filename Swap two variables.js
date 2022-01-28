ae491bb4d4af61d6ede70d501f685cb65b39014c
SahanaRavikumar-13 <63450434+SahanaRavikumar-13@users.noreply.github.com>
2022-01-28 13:09:25 +0530

    Add files via upload

---
+++ Swap two variables.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
